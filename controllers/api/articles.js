22483670477c03b16cd484dec1fa0989805b5174
shualton <shualton@gmail.com>
2019-07-22 22:30:58 -0700

    changed some of articles.js

--- controllers/api/articles.js
+++ controllers/api/articles.js
@@ -4,9 +4,8 @@ cheerio = require('cheerio');
 Article = require('../../models/article');
 
 router.get('/', function(req, res) {
-    Article
-        .find({})
-        .exec(function(error, docs) {
+    Article.find({})
+    Article.exec(function(error, docs) {
             if (error) {
                 console.log(error);
                 res.status(300);
@@ -17,12 +16,11 @@ router.get('/', function(req, res) {
 });
 
 router.get('/saved', function(req, res) {
-    Article
-        .find({})
-        .where('saved').equals(true)
-        .where('deleted').equals(false)
-        .populate('notes')
-        .exec(function(error, docs) {
+    Article.find({})
+    Article.where('saved').equals(true)
+    Article.where('deleted').equals(false)
+    Article.populate('notes')
+    Article.exec(function(error, docs) {
             if (error) {
                 console.log(error);
                 res.status(300);
@@ -48,10 +46,9 @@ router.post('/save/:id', function(req, res) {
 });
 
 router.get('/deleted', function(req, res) {
-    Article
-        .find({})
-        .where('deleted').equals(true)
-        .exec(function(error, docs) {
+        Article.find({})
+        Article.where('deleted').equals(true)
+        Article.exec(function(error, docs) {
             if (error) {
                 console.log(error);
                 res.status(300);
@@ -92,34 +89,23 @@ router.delete('/:id', function(req, res) {
 
 router.get('/scrape', function(req, res, next) {
     request('https://www.nytimes.com/', function(error, response, html) {
-        let $ = cheerio.load(html);
-        let results = [];
-        $('tr.athing td.title').each(function(i, e) {
-            let title = $(this).children('a').text(),
-                link = $(this).children('a').attr('href'),
-                single = {};
-            if (link !== undefined && link.includes('http') &&  title !== '') {
-                single = {
-                    title: title,
-                    link: link
-                };
-                // create new article
-                let entry = new Article(single);
-                // save to database
-                entry.save(function(err, doc) {
-                    if (err) {
-                        if (!err.errors.link) {
-                            console.log(err);
-                        }
-                    } else {
-                        console.log('new article added');
-                        
-                    }
-                });
-            }
-        });
-        next();
+        var $ = cheerio.load(response.data);
+        var results = {};
+        result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
     });
+
+    db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
 }, function(req, res) {
     res.redirect('/');
 });
