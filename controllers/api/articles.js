6e5f4e67fd14ecafbc5fc49bf6d15f87be4e408b
shualton <shualton@gmail.com>
2019-07-22 22:18:13 -0700

    added to articles.js

--- controllers/api/articles.js
+++ controllers/api/articles.js
@@ -60,3 +60,68 @@ router.get('/deleted', function(req, res) {
             }
         });
 });
+
+router.delete('/remove/:id', function(req, res) {
+    Article.findByIdAndUpdate(req.params.id,
+        { $set: { deleted: true } },
+        { new: true },
+        function(error, doc) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.redirect('/');
+            }
+        });
+});
+
+router.delete('/:id', function(req, res) {
+    Article.findByIdAndUpdate(req.params.id,
+        { $set: { deleted: true} },
+        { new: true },
+        function(error, doc) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.redirect('/saved');
+            }
+        }
+    );
+});
+
+router.get('/scrape', function(req, res, next) {
+    request('https://www.nytimes.com/', function(error, response, html) {
+        let $ = cheerio.load(html);
+        let results = [];
+        $('tr.athing td.title').each(function(i, e) {
+            let title = $(this).children('a').text(),
+                link = $(this).children('a').attr('href'),
+                single = {};
+            if (link !== undefined && link.includes('http') &&  title !== '') {
+                single = {
+                    title: title,
+                    link: link
+                };
+                // create new article
+                let entry = new Article(single);
+                // save to database
+                entry.save(function(err, doc) {
+                    if (err) {
+                        if (!err.errors.link) {
+                            console.log(err);
+                        }
+                    } else {
+                        console.log('new article added');
+                        
+                    }
+                });
+            }
+        });
+        next();
+    });
+}, function(req, res) {
+    res.redirect('/');
+});
+
+module.exports = router;
